014708fa89b1c1adf26a3f7b92806d53feaf65dd
alex99q <alex99q@abv.bg>
2017-02-12 01:44:13 +0200

    Added additional projects

---
+++ Diamond/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int LeftRight = (n - 1) / 2;
+            int mid = 0;
+
+            if (n % 2 == 1)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if(i < n / 2)
+                    {
+                        mid = n - 2 * LeftRight - 2;
+
+                        Console.Write(new string('-', LeftRight));
+                        Console.Write("*");
+                        if (mid >= 0) Console.Write(new string('-', mid));
+                        if (mid >= 0) Console.Write("*");
+                        Console.WriteLine(new string('-', LeftRight));
+
+                        LeftRight -= 1;
+                    }
+                    else if (i >= n / 2)
+                    {
+                        mid = n - 2 * LeftRight - 2;
+
+                        Console.Write(new string('-', Math.Abs(LeftRight)));
+                        Console.Write("*");
+                        if (mid >= 0) Console.Write(new string('-', mid));
+                        if (mid >= 0) Console.Write("*");
+                        Console.WriteLine(new string('-', Math.Abs(LeftRight)));
+
+                        LeftRight += 1;
+                    }
+                }
+            }
+
+            if (n % 2 == 0)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (i < n / 2)
+                    {
+                        mid = n - 2 * LeftRight - 2;
+
+                        Console.Write(new string('-', LeftRight));
+                        Console.Write("*");
+                        if (mid >= 0) Console.Write(new string('-', mid));
+                        Console.Write("*");
+                        Console.WriteLine(new string('-', LeftRight));
+
+                        LeftRight -= 1;
+                    }
+                    else if (i == n / 2)
+                    {
+                        LeftRight = 1;
+                        mid = n - 2 * LeftRight - 2;
+
+                        Console.Write(new string('-', Math.Abs(LeftRight)));
+                        Console.Write("*");
+                        if (mid >= 0) Console.Write(new string('-', mid));
+                        Console.Write("*");
+                        Console.WriteLine(new string('-', Math.Abs(LeftRight)));
+
+                        LeftRight += 1;
+                    }
+                    else
+                    {
+                        mid = n - 2 * LeftRight - 2;
+
+                        Console.Write(new string('-', Math.Abs(LeftRight)));
+                        Console.Write("*");
+                        if (mid >= 0) Console.Write(new string('-', mid));
+                        Console.Write("*");
+                        Console.WriteLine(new string('-', Math.Abs(LeftRight)));
+
+                        LeftRight += 1;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ House/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int row = 0;
+
+            for (int i = 0; i < ((n + 1) / 2); i++)
+            {
+                if (n % 2 == 0 && i == 0)
+                {
+                    Console.Write(new string('-', (n / 2) - 1));
+                    Console.Write(new string('*', 2));
+                    Console.WriteLine(new string('-', (n / 2) - 1));
+                    row += 2;
+                }
+                else if (n % 2 == 1 && i == 0)
+                {
+                    Console.Write(new string('-', (n - 1) / 2));
+                    Console.Write(new string('*', 1));
+                    Console.WriteLine(new string('-', (n - 1) / 2));
+                    row += 1;
+                }
+                else if (i == (((n + 1) / 2)) - 1)
+                {
+                    Console.WriteLine(new string('*', row));
+                }
+                else
+                {
+                    Console.Write(new string('-', Convert.ToInt32((n - row) / 2)));
+                    Console.Write(new string('*', row));
+                    Console.WriteLine(new string('-', Convert.ToInt32((n - row) / 2)));
+                }
+                row += 2;
+            }
+
+            for (int j = 0; j < n / 2; j++)
+            {
+                Console.Write("|");
+                Console.Write(new string('*', n - 2));
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
